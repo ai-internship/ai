baa6c5cd085da2c8e5e2b0a4f4a105594452ba18
Mickey248 <mickeynaja246@gmail.com>
2018-10-09 15:29:30 +0700

    Add files via upload

---
+++ day5.ipython/pulse.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr = np.arange(0,10)
+
+arr + arr
+
+# please by step
+# 1. press cmd
+# 2. write and press ipython
+# 3. write and press address a pulse.py
+# 4. write and press run pulse.py
+# 5. white and press arr
